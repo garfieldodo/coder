e0d13a2eb81a8c150016bc7e0733738bb4c2a24a
leo <64378594+garfieldodo@users.noreply.github.com>
2020-04-27 13:23:54 +0200

    Update decoder.py

--- decoder.py
+++ decoder.py
@@ -22,9 +22,53 @@ fword = len(start)
 sword = len(middle)
 tword = len(end)
 
-#this seperates each word into 2 parts: the first letter and the restsstart = start [0:1]
-estart = start [1:fword]
+estart = start [fword:0]
+emiddle = middle [sword:0]
+eend = end [tword:0]
 
-emiddle = middle [1:sword]
+bob = len(estart)
+jeff = len(emiddle)
+tom = len(eend)
 
-eend = end [1:tword]
+print (estart)
+print (emiddle)
+print (eend)
+
+pstart = estart [0:2]
+sstart = estart [0:3]
+bstart = estart [3:4]
+
+pmiddle = emiddle [0:2]
+smiddle = emiddle [0:3]
+bmiddle = emiddle [3:4]
+
+pend = eend [0:2]
+send = eend [0:3]
+bend = eend [3:4]
+
+if sstart == ("yaw"):
+    newword = estart [3:bob]
+else:
+    if pstart == ("ya"):
+        newword = estart [3:bob] + bstar
+
+if smiddle == ("yaw"):
+    twonewword = emiddle [3:jeff]
+else:
+    if pmiddle == ("ya"):
+        twonewword = emiddle [3:bob] + bmiddle
+
+if send == ("yaw"):
+    thirdnewword = eend [3:bob]
+else:
+    if pend == ("ya"):
+        thirdnewword = eend [3:bob] + bend
+
+first = len(newword)
+second = len(twonewword)
+third = len(thirdnewword)
+
+newsentence = newword [first:0] + twonewword [secnd:0] + thirdnewword [third:0]
+newsentence = newsentence.lower()
+
+print (newsentence)
